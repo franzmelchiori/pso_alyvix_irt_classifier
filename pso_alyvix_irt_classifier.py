c74b09155505c0843aad84287f12bb698cace604
Francesco Melchiori <franzmelchiori@gmail.com>
2018-05-24 17:35:19 +0200

    add Particle

--- pso_alyvix_irt_classifier.py
+++ pso_alyvix_irt_classifier.py
@@ -22,6 +22,7 @@
 """
 
 
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -32,12 +33,15 @@ class ParameterSampling:
                  sampling_type='linear'):
         self.lowerbound = lowerbound
         self.upperbound = upperbound
-        self.sampling_type = sampling_type
-        self.samples_amount = samples_amount
-        if type(float()) in (type(self.lowerbound), type(self.upperbound)):
-            self.samples_type = np.float64
+        if samples_amount <= 255:
+            self.samples_amount = samples_amount
         else:
-            self.samples_type = np.int64
+            self.samples_amount = 255
+        self.sampling_type = sampling_type
+        if type(float()) in (type(self.lowerbound), type(self.upperbound)):
+            self.samples_type = np.float16
+        else:
+            self.samples_type = np.int16
         self.samples = np.zeros(shape=self.samples_amount,
                                 dtype=self.samples_type)
         if sampling_type == 'linear':
@@ -61,47 +65,88 @@ class ParameterSampling:
         return self.samples
 
 
-class PSO:
+class Particle:
 
-    def __init__(self, parameters):
-        self.parameters = parameters
-        self.solution_space = np.zeros(1, dtype=np.int64)
-        self.parameters_types = np.dtype('int64')
-        self.get_parameters_types()
-        self.build_solution_space()
+    def __init__(self, solution_dimensions, inertial_weight=1.,
+                 cognitive_weight=1., social_weight=1):
+        self.solution_dimensions = solution_dimensions
+        self.weight = np.ones([3], dtype=np.float16)
+        self.weight[0] = inertial_weight
+        self.weight[1] = cognitive_weight
+        self.weight[2] = social_weight
+        self.random = np.ones([3], dtype=np.float16)
+        self.set_random()
+        self.speed = self.init_position()
+        self.position = self.init_position()
+        self.best = self.init_position()
 
     def __repr__(self):
         print_message = ''
-        print_message += '{0}\n'.format(self.parameters_types)
-        print_message += '{0}\n'.format(self.solution_space)
+        print_message += "Particle weights: '{0}'\n".format(self.weight)
+        print_message += "Particle randoms: '{0}'\n".format(self.random)
+        print_message += "Particle speed: '{0}'\n".format(self.speed)
+        print_message += "Particle position: '{0}'\n".format(self.position)
+        print_message += "Particle best position: '{0}'".format(self.best)
         return print_message
 
-    def get_parameters_types(self):
-        parameters_types = ''
-        for parameter in self.parameters:
-            if parameter.samples.dtype is np.dtype('float'):
-                parameters_types += 'f8,'
-            else:
-                parameters_types += 'i8,'
-        self.parameters_types = np.dtype(parameters_types[:-1])
-        return self.parameters_types
+    def set_random(self):
+        self.random[1] *= np.array([random.random()], dtype=np.float16)
+        self.random[2] *= np.array([random.random()], dtype=np.float16)
 
-    def build_solution_space(self):
-        parameters_sizes = []
-        for parameter in self.parameters:
-            parameters_sizes.append(parameter.samples.size)
-        self.solution_space = np.zeros(parameters_sizes)
-        return self.solution_space
+    def init_position(self):
+        return np.zeros([self.solution_dimensions], dtype=np.uint8)
+
+
+class PSO:
+
+    def __init__(self, parameters, particle_amount):
+        self.parameters = parameters
+        self.solution_dimensions = len(self.parameters)
+        # self.parameters_types = np.dtype(np.byte)
+        # self.get_parameters_types()
+        self.particle_amount = particle_amount
+        self.particle_space = np.zeros(0, dtype=np.byte)
+        self.init_particle_space()
+        self.result_space = np.zeros(0, dtype=np.byte)
+        self.build_result_space()
+
+    def __repr__(self):
+        print_message = ''
+        # print_message += '{0}\n'.format(self.parameters_types)
+        for particle in self.particle_space:
+            print_message += '{0}\n\n'.format(particle)
+        print_message += '{0}\n'.format(self.result_space)
+        return print_message
+
+    # def get_parameters_types(self):
+    #     parameters_names = ['p{0}'.format(parameter_number)
+    #                         for parameter_number in range(len(self.parameters))]
+    #     parameters_types = [parameter.samples_type
+    #                         for parameter in self.parameters]
+    #     self.parameters_types = np.dtype(zip(parameters_names,
+    #                                          parameters_types))
+    #     return self.parameters_types
+
+    def init_particle_space(self):
+        self.particle_space = [Particle(self.solution_dimensions)
+                               for particle_number
+                               in range(self.particle_amount)]
+        return self.particle_space
+
+    def build_result_space(self):
+        parameters_sizes = [parameter.samples.size
+                            for parameter in self.parameters]
+        self.result_space = np.zeros(parameters_sizes, dtype=np.float16)
+        return self.result_space
 
 
 def main():
-    # pso_test = PSO()
-    # print(pso_test)
     param_1 = ParameterSampling(0, 2, 5)
-    param_2 = ParameterSampling(0., 10, 5)
-    params = [param_1, param_2]
-    pso = PSO(params)
-    print(pso)
+    param_2 = ParameterSampling(0., 10, 7)
+    param_3 = ParameterSampling(0, 1, 2)
+    params = [param_1, param_2, param_3]
+    pso_test = PSO(params, 2)
+    print(pso_test)
 
 
 if __name__ == '__main__':
