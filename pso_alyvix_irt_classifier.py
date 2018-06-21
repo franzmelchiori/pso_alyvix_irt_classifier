dcc48af705c90a4f7dfc55d4a27c2acdfb6d1acc
Francesco Melchiori <franzmelchiori@gmail.com>
2018-06-21 17:39:33 +0200

    total revision and some refactoring

--- pso_alyvix_irt_classifier.py
+++ pso_alyvix_irt_classifier.py
@@ -24,15 +24,11 @@
 
 import random
 import numpy as np
-import scipy.special as sps
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cbook
-from matplotlib import cm
-from matplotlib.colors import LightSource
 import matplotlib.animation as animation
+from matplotlib import cbook
 
 
 class ParameterSampling:
@@ -75,22 +71,25 @@ class ParameterSampling:
 
 class Particle:
 
-    def __init__(self, gain_function, solution_space_sizes, inertial_weight=.1,
-                 cognitive_weight=1., social_weight=.5):
+    def __init__(self, gain_function, parameters, solution_space_sizes,
+                 inertial_weight=1., cognitive_weight=1., social_weight=1.,
+                 serial_number=0):
         self.gain_function = gain_function
-        self.solution_space_sizes = solution_space_sizes
-        self.solution_dimensions = len(self.solution_space_sizes)
+        self.parameters = parameters
+        self.solution_sizes = solution_space_sizes
         self.weight = np.ones([3], dtype=np.float16)
         self.weight[0] = inertial_weight
         self.weight[1] = cognitive_weight
         self.weight[2] = social_weight
+        self.serial_number = serial_number
         self.random = np.ones([3], dtype=np.float16)
-        self.set_random()
         self.speed = self.init_position()
         self.position = self.init_position()
         self.best = self.init_position()
+        self.best_value = False
         self.best_swarm = self.init_position()
-        self.samples = {}
+        self.best_swarm_value = False
+        self.samples = []
 
     def __repr__(self):
         print_message = ''
@@ -102,90 +101,99 @@ class Particle:
         return print_message
 
     def set_random(self):
-        self.random[1] *= np.array([random.random()], dtype=np.float16)
-        self.random[2] *= np.array([random.random()], dtype=np.float16)
+        self.random = np.ones([3], dtype=np.float16)
+        self.random[1:] = np.random.random((1, 2))
 
     def init_position(self):
-        return np.zeros([self.solution_dimensions], dtype=np.int16)
+        return np.zeros([len(self.solution_sizes)], dtype=np.int16)
 
-    def set_position(self, position, init=False):
+    def set_position(self, position):
         self.position = position
-        if init:
-            self.best_swarm = position
 
     def set_best_swarm(self, position):
         self.best_swarm = position
 
-    def bouncing(self, component_displace):
-        for (dimension_number, dimension_upperbound) in enumerate(
-                self.solution_space_sizes):
-            intertial_rebound = abs(component_displace[dimension_number])
-            (rebound_number, rebound_displace) = divmod(
-                intertial_rebound, dimension_upperbound)
-            if rebound_number % 2 is not False:
-                component_displace[dimension_number] = rebound_displace
-            else:
-                component_displace[dimension_number] = dimension_upperbound - \
-                                                       rebound_displace
-
-    def quantize_position(self):
+    def quantize_vector(self, vector):
         """
             integer position
             from position = 0
-            to position < self.solution_space_sizes[<dim>]
+            to position < self.solution_sizes[<dim>]
         """
-        self.position = np.array(self.position + 0.5, dtype=np.int16)
-        position_control = self.position < self.solution_space_sizes
-        position_valid = self.position * np.equal(position_control, True)
-        position_correct = (self.solution_space_sizes - 1) * np.equal(
+        vector_expanded = np.array(vector + 0.5, dtype=np.int16)
+        position_control = vector_expanded < self.solution_sizes
+        position_valid = vector_expanded * np.equal(position_control, True)
+        position_correct = (self.solution_sizes - 1) * np.equal(
             position_control, False)
-        self.position = np.array(position_valid + position_correct,
-                                 dtype=np.int16)
+        vector = np.array(position_valid + position_correct, dtype=np.int16)
+        return vector
+        # self.position = np.array(self.position + 0.5, dtype=np.int16)
+        # position_control = self.position < self.solution_sizes
+        # position_valid = self.position * np.equal(position_control, True)
+        # position_correct = (self.solution_sizes - 1) * np.equal(
+        #     position_control, False)
+        # self.position = np.array(position_valid + position_correct,
+        #                          dtype=np.int16)
 
     def sample_gain_function(self):
-        position = self.position[0], self.position[1]
-        value = self.gain_function(*position)
-        try:
-            if value > max(self.samples):
-                self.best = position
-        except ValueError:
-            pass
-        self.samples = self.position[0], self.position[1], value
+        function_parameters = []
+        sample_coordinate = []
+        for dimension, coordinate in enumerate(self.position):
+            function_parameters.append(
+                self.parameters[dimension].samples[coordinate])
+            sample_coordinate.append(coordinate)
+        value = self.gain_function(function_parameters)
+        sampled_values = [sample[-1] for sample in self.samples]
+        if (self.best_value is False) or (value >= max(sampled_values)):
+            self.best = self.position
+            self.best_value = value
+        sample = sample_coordinate
+        sample.append(value)
+        sample = tuple(sample)
+        self.samples.append(sample)
+        return sample
 
     def perturb(self):
-        intertial_displace = self.speed - 0
-        cognitive_displace = self.best - self.position
-        social_displace = self.best_swarm - self.position
+        if self.best_value is False:
+            intertial_displace = self.init_position()
+            cognitive_displace = self.init_position()
+            social_displace = self.init_position()
+        else:
+            intertial_displace = self.speed - 0
+            cognitive_displace = self.best - self.position
+            social_displace = self.best_swarm - self.position
+        self.set_random()
         intertial_term = self.weight[0] * self.random[0] * intertial_displace
         cognitive_term = self.weight[1] * self.random[1] * cognitive_displace
         social_term = self.weight[2] * self.random[2] * social_displace
-        self.bouncing(intertial_term)
-        self.bouncing(cognitive_term)
-        self.bouncing(social_term)
         self.speed = intertial_term + cognitive_term + social_term
         self.position = self.position + self.speed
-        self.quantize_position()
-        self.sample_gain_function()
+        self.position = self.quantize_vector(self.position)
+        sample = self.sample_gain_function()
+        return sample
 
 
 class PSO:
 
-    def __init__(self, gain_function, parameters, particle_amount=3):
+    def __init__(self, gain_function, parameters, iterations, particle_amount=3,
+                 inertial_weight=1., cognitive_weight=1., social_weight=1.):
         self.gain_function = gain_function
         self.parameters = parameters
+        self.iterations = iterations
         self.particle_amount = particle_amount
-        self.parameter_types = [parameter.samples_type
-                                for parameter in self.parameters]
-        self.solution_space_sizes = np.array(
-            [parameter.samples.size for parameter in self.parameters],
-            dtype=np.int16)
-        self.solution_dimensions = len(self.solution_space_sizes)
-        self.particle_space = np.zeros(0, dtype=np.byte)
-        self.init_particle_space()
-        self.particle_result = Particle(self.gain_function,
-                                        self.solution_space_sizes)
-        self.result_space = np.zeros(0, dtype=np.byte)
-        self.build_result_space()
+        self.inertial_weight = inertial_weight
+        self.cognitive_weight = cognitive_weight
+        self.social_weight = social_weight
+        self.solution_sizes = np.array([parameter.samples.size
+                                        for parameter in self.parameters],
+                                       dtype=np.int16)
+        self.particle_space = self.init_particle_space()
+        self.particle_result = Particle(
+            gain_function=self.gain_function, parameters=self.parameters,
+            solution_space_sizes=self.solution_sizes,
+            inertial_weight=self.inertial_weight,
+            cognitive_weight=self.cognitive_weight,
+            social_weight=self.social_weight, serial_number='r')
+        self.result_space = self.init_result_space()
 
     def __repr__(self):
         print_message = ''
@@ -196,36 +204,51 @@ class PSO:
         return print_message
 
     def init_particle_space(self):
-        self.particle_space = [Particle(self.gain_function,
-                                        self.solution_space_sizes)
-                               for particle_number
-                               in range(self.particle_amount)]
+        self.particle_space = [Particle(
+            gain_function=self.gain_function, parameters=self.parameters,
+            solution_space_sizes=self.solution_sizes,
+            inertial_weight=self.inertial_weight,
+            cognitive_weight=self.cognitive_weight,
+            social_weight=self.social_weight, serial_number=particle_number)
+            for particle_number in range(self.particle_amount)]
         for particle in self.particle_space:
-            position = np.array([random.randint(0, coordinate_size-1)
+            position = np.array([random.randint(0, coordinate_size - 1)
                                  for coordinate_size
-                                 in self.solution_space_sizes],
+                                 in self.solution_sizes],
                                 dtype=np.int16)
-            particle.set_position(position, True)
+            particle.set_position(position)
         return self.particle_space
 
-    def build_result_space(self):
+    def init_result_space(self):
         parameters_sizes = [parameter.samples.size
                             for parameter in self.parameters]
         self.result_space = np.zeros(parameters_sizes, dtype=np.float16)
         return self.result_space
 
-    def iter_particle_swarm(self, iterations=30):
+    def iter_particle_swarm(self):
         particles_data = []
         for particle in self.particle_space:
-            particle_data = np.empty((self.solution_dimensions + 1, iterations))
+            particle_data = np.empty((len(self.solution_sizes) + 1, self.iterations))
             particles_data.append(particle_data)
-        for i in range(iterations):
+        for i in range(self.iterations):
             print('*** start iter {0}***'.format(i+1))
             for particle, particle_data in zip(self.particle_space,
                                                particles_data):
-                particle.perturb()
-                particle_data[:, i] = particle.samples
-                print(particle.samples)
+                sample = particle.perturb()
+                value = sample[-1]
+                sampled_best_value = self.particle_result.best_swarm_value
+                if value > sampled_best_value:
+                    self.particle_result.best_swarm = particle.position
+                    self.particle_result.best_swarm_value = value
+                particle_data[:, i] = sample[0], sample[1], value
+                print('particle sample: {}, {}, {}'.format(
+                    sample[0], sample[1], value))
+            for particle in self.particle_space:
+                particle.set_best_swarm(self.particle_result.best_swarm)
+            print('best swarm position: {}'.format(
+                self.particle_result.best_swarm))
+            print('best swarm value: {}'.format(
+                self.particle_result.best_swarm_value))
             print('')
         return particles_data
 
@@ -244,8 +267,8 @@ class Mountain:
             region = np.s_[0:param_1_dim, 0:param_2_dim]
             self.x, self.y, self.z = x[region], y[region], z[region]
 
-    def altitude_function(self, param_1, param_2):
-        return self.z[param_1, param_2]
+    def altitude_function(self, params):
+        return self.z[params[0], params[1]]
 
     def surface_plot_2d(self):
         fig, ax = plt.subplots()
@@ -256,25 +279,18 @@ class Mountain:
         p = particle_data
         iterations = len(particle_data[0])
         dimensions = 3
-        lineData = np.empty((dimensions, iterations))
+        trajectory_data = np.empty((dimensions, iterations))
         for i in range(iterations):
-            lineData[:, i] = self.x[int(p[0, i]), int(p[1, i])], \
-                             self.y[int(p[0, i]), int(p[1, i])], \
-                             self.z[int(p[0, i]), int(p[1, i])]
-        return lineData
+            trajectory_data[:, i] = self.x[int(p[0, i]), int(p[1, i])], \
+                                    self.y[int(p[0, i]), int(p[1, i])], \
+                                    self.z[int(p[0, i]), int(p[1, i])]+10
+        return trajectory_data
 
-    def update_lines(self, num, dataLines, lines):
-        for line, data in zip(lines, dataLines):
+    def update_trajectories(self, num, trajectories_data, lines):
+        for line, data in zip(lines, trajectories_data):
             line.set_data(data[0:2, :num])
             line.set_3d_properties(data[2, :num])
         return lines
-
-    # def update_points(self, i, particles_data, points):
-    #     for points, particle_data in zip(points, particles_data):
-    #         points.set_3d_properties(self.x[int(particle_data[0][i]), int(particle_data[1][i])],
-    #                                  self.y[int(particle_data[0][i]), int(particle_data[1][i])],
-    #                                  self.z[int(particle_data[0][i]), int(particle_data[1][i])] + 10)
-    #     return points
 
     def surface_plot_3d(self, particles_data):
         iterations = len(particles_data[0][0])
@@ -283,39 +299,30 @@ class Mountain:
 
         data = [self.particle_trajectory(particle_data)
                 for particle_data in particles_data]
-        lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0]
+        lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1]+10)[0]
                  for dat in data]
-
-        line_ani = animation.FuncAnimation(fig=fig,
-                                           func=self.update_lines,
-                                           frames=iterations,
-                                           fargs=(data, lines),
-                                           interval=50,
-                                           blit=False)
-
         # points = [ax.scatter3D(self.x[int(p[0][1]), int(p[1][1])],
         #                        self.y[int(p[0][1]), int(p[1][1])],
         #                        self.z[int(p[0][1]), int(p[1][1])]+10,
         #                        s=30, c='r') for p in particles_data]
-        # line_ani = animation.FuncAnimation(fig=fig,
-        #                                    func=self.update_points,
-        #                                    frames=5,
-        #                                    fargs=(particles_data, points),
-        #                                    interval=24,
-        #                                    repeat=False,
-        #                                    blit=False)
 
+        animate_trajectories = animation.FuncAnimation(
+            fig=fig, func=self.update_trajectories, frames=iterations,
+            fargs=(data, lines), interval=1000, blit=False)
         plt.show()
 
 
 def main():
     param_1 = ParameterSampling(0, 49, 50)
     param_2 = ParameterSampling(0, 49, 50)
+    param_3 = ParameterSampling(0, 99, 100)
     params = [param_1, param_2]
     # print(params)
     fnc = Mountain(50, 50)
     gain_function = fnc.altitude_function
-    pso = PSO(gain_function, params, 10)
+    pso = PSO(gain_function=gain_function, parameters=params, iterations=10,
+              particle_amount=10, inertial_weight=.5, cognitive_weight=.5,
+              social_weight=1.)
     particles_data = pso.iter_particle_swarm()
     fnc.surface_plot_3d(particles_data)
 
